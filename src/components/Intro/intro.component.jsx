b1c79e33b28d61d09edd2534449d3730ab580937
Jack Nortje <jack@bluegrassdigital.com>
2023-05-29 11:36:33 +0200

    Up2date

--- src/components/Intro/intro.component.jsx
+++ src/components/Intro/intro.component.jsx
@@ -1,18 +1,19 @@
 
-import './intro.css'
+import './intro.css';
+import React from 'react';
 function Intro() { 
 
     return (
-      <section class="container intro">
+      <section class=" container intro">
         <div className="introText">
-          <h1 class="introHeading1">Howzit! welcome to the portfolio page of</h1>
-          <h2 class="introHeading2">Jack Liam Nortje</h2>
-          <h3 class="introHeading3">Software Developer/ Tester</h3>
-          <p class="introParagraph">I decided to switch careers in 2021 and followed the route of Front End Web Devlopement. I am currently working as a Quality Assurance Tester 
-              for Bluegrass Digital.
-          </p>
-          <a href="https://github.com/JackNortje" class="github-projects" target="_blank"><button class="githubButton">Github Repos</button></a>
-          </div>
+
+            <h1 class="introHeading1">Howzit! welcome to the portfolio page of</h1>
+            <h2 class="introHeading2">Jack Liam Nortje</h2>
+            <h3 class="introHeading3">QA Engineer and aspiring UI UX Designer</h3>
+            <p class="introParagraph">I decided to switch careers in 2021 and followed the route of Front End Web Devlopement. I am currently working as a Quality Assurance Tester 
+                for Bluegrass Digital.</p>
+            <a href="https://github.com/JackNortje" class="github-projects" target="_blank" rel="noopener noreferrer" ><button class="githubButton" >Github Repos</button></a>
+        </div>
       </section>
    
 
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,4 +1,5 @@
 import "./projects.css";
+import React from 'react';
 
 function Projects() { 
 
@@ -9,23 +10,23 @@ function Projects() {
                         <div className="col-md"><span class="project">
                         <h4>First landing page</h4>
                         <p>Html5, CSS</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/web-dev/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/web-dev" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/web-dev/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/web-dev" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span></div>
                         <div className="col-md">
                         <span class="project">
                         <h4>Cape Town Beaches</h4>
                         <p>Html5, CSS</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/cape-town-best-beaches/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/cape-town-best-beaches" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/cape-town-best-beaches/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/cape-town-best-beaches" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>
                         </div>
                         <div className="col-md">
                         <span class="project">
                         <h4>Jack's Bike Shop</h4>
                         <p>Html5, CSS</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/jacks-bicycle-shop/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/jacks-bicycle-shop" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/jacks-bicycle-shop/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/jacks-bicycle-shop" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>
                         </div>
                         </div>
@@ -34,24 +35,24 @@ function Projects() {
                         <span class="project">
                         <h4>FEDSA Dev Project</h4>
                         <p>Html5, CSS, Figma, Trelo</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/fedsa-responsive-design/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/fedsa-responsive-design" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/fedsa-responsive-design/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/fedsa-responsive-design" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>
                         </div>
                         <div className="col-md">
                         <span class="project">
                         <h4>First JS game</h4>
                         <p>Html5, CSS, Javascript</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/TOP_BlackJack/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/TOP_BlackJack" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/TOP_BlackJack/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/TOP_BlackJack" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>   
                         </div>
                         <div className="col-md">
                         <span class="project">
                         <h4>Book Library</h4>
                         <p>Html5, CSS, Javascript</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/book-library/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/book-library" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/book-library/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/book-library" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>   
                         </div>
                         </div>
@@ -60,24 +61,24 @@ function Projects() {
                         <span class="project">
                         <h4>Pixel Drawing Game</h4>
                         <p>Html5, CSS, Javascript</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/block-game/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/block-game" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/block-game/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/block-game" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>   
                         </div>
                         <div className="col-md">
                         <span class="project">
                         <h4>Calculator</h4>
                         <p>Html5, CSS, Javascript</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/calculator-top/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/calculator-top" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/calculator-top/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/calculator-top" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span>    
                         </div>
                         <div className="col-md">
                         <span class="project">
                         <h4>X's and O's</h4>
                         <p>Html5, CSS, Javascript</p>
-                        <button class="live-view1"><a href="https://jacknortje.github.io/tictactoe/" target="_blank">Live Preview</a></button>
-                        <button class="Github-Repo"><a href="https://github.com/JackNortje/tictactoe" target="_blank">Github-Repo</a></button>
+                        <button class="live-view1"><a href="https://jacknortje.github.io/tictactoe/" target="_blank" rel="noopener noreferrer" >Live Preview</a></button>
+                        <button class="Github-Repo"><a href="https://github.com/JackNortje/tictactoe" target="_blank" rel="noopener noreferrer" >Github-Repo</a></button>
                     </span> 
                     </div>  
                         </div>
